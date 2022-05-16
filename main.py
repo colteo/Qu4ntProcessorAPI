6b55b6ffd7b8387e929b921d7898975554c7342d
Simone Greco <simonegreco.inf@gmail.com>
2022-05-16 22:30:24 +0200

    processor paths managed for executing main.py from api

--- main.py
+++ main.py
@@ -3,11 +3,24 @@ from bson.json_util import dumps
 from bson.objectid import ObjectId
 from flask import jsonify, request
 from subprocess import Popen
+import os
+from sys import platform
+
+
+def get_qu4nt_processor_paths():
+    this_file_path = os.path.dirname(os.path.dirname(__file__))
+    processor_path = os.path.abspath(os.path.join(this_file_path, os.pardir, "Qu4ntProcessor", ""))
+    processor_main_path = os.path.join(processor_path, "main.py")
+    if platform == "linux" or platform == "linux2":
+        venv_path = os.path.abspath(os.path.join(processor_path, "venv", "bin", "python3", ""))
+    elif platform == "win32":
+        venv_path = os.path.abspath(os.path.join(processor_path, "venv", "Scripts", "python.exe", ""))
+    return venv_path, processor_main_path
 
 
 def qu4nt_launcher(settings_object_id):
-    qu4nt_root_path = "/home/simone/Scrivania/trading/codice_nuovo/ok/Qu4ntProcessor/"
-    command = "python3 {}main.py --settings_id {}".format(qu4nt_root_path, settings_object_id)
+    venv_path, processor_main_path = get_qu4nt_processor_paths()
+    command = "{} {} --settings_id {}".format(venv_path, processor_main_path, settings_object_id)
     proc = Popen(command, shell=True,
                  stdin=None, stdout=None, stderr=None, close_fds=True)
     return proc.pid
